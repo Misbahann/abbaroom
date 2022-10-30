2ee7ea0808d9086324f366c6884859d812699adc
LAPTOP-5ID722CU\Misbah Ann <misbahvinx99@gmail.com>
2022-10-30 20:47:06 +0700

    Update convert from figma to android studio

---
+++ app/src/main/java/com/example/abbauiproject/layer2project.java
@@ -0,0 +1,14 @@
+package com.example.abbauiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layer2project extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layer2project);
+    }
+}
